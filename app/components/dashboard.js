170f35589c427d246b71fb0a0ae56886532f82e4
Roy McFarland <roy.mcfarland@gmail.com>
2015-04-26 00:05:45 -0600

    add dashboard component

---
+++ app/components/dashboard.js
@@ -0,0 +1,81 @@
+var React = require('react-native');
+
+var {
+	Text,
+	View,
+	StyleSheet,
+	Image,
+	TouchableHighlight
+} = React;
+
+var styles = StyleSheet.create({
+	container: {
+		marginTop: 65,
+		flex: 1
+	},
+	image: {
+		height: 350
+	},
+	buttonText: {
+		fontSize: 24,
+		color: 'white',
+		alignSelf: 'center'
+	}
+});
+
+class Dashboard extends React.Component{
+	makeBackground(btn){
+		var obj = {
+			flexDirection: 'row',
+			alignSelf: 'stretch',
+			justifyContent: 'center',
+			flex: 1
+		}
+
+		if(btn === 0){
+			obj.backgroundColor = '#48BBEC';
+		} else if (btn === 1){
+			obj.backgroundColor = '#E77AAE'
+		} else {
+			obj.backgroundColor = '#758BF4'
+		}
+
+		return obj;
+	}
+	goToProfile(){
+		
+	}
+	goToRepos(){
+
+	}
+	goToNotes(){
+
+	}
+	render(){
+		return (
+			<View style={styles.container}>
+				<Image source={{uri: this.props.userInfo.avatar_url}} style={styles.image} />
+				<TouchableHighlight
+					style={this.makeBackground(0)}
+					onPress={this.goToProfile.bind(this)}
+					underlayColor='#88D4F5'>
+						<Text style={styles.buttonText}> View Profile </Text>
+				</TouchableHighlight>
+				<TouchableHighlight
+					style={this.makeBackground(1)}
+					onPress={this.goToRepos.bind(this)}
+					underlayColor='#88D4F5'>
+						<Text style={styles.buttonText}> View Repos </Text>
+				</TouchableHighlight>
+				<TouchableHighlight
+					style={this.makeBackground(2)}
+					onPress={this.goToNotes.bind(this)}
+					underlayColor='#88D4F5'>
+						<Text style={styles.buttonText}> View Notes </Text>
+				</TouchableHighlight>
+			</View>
+		)
+	}
+};
+
+module.exports = Dashboard;
